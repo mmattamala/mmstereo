57548ff31d4eb5d9b6b85b73529036a039565abe
Matias Mattamala <matias@robots.ox.ac.uk>
2023-09-21 14:05:51 +0100

    dockerfile

---
+++ mmstereo/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="mmstereo",
+    version="0.0.0",
+    author="Krishna Shankar, Mark Tjersland, Jeremy Ma, Kevin Stone, Max Bajracharya",
+    author_email="matias@robots.ox.ac.uk",
+    packages=["mmstereo"],
+    package_dir={"": "mmstereo"},
+    python_requires=">=3.8",
+    description="mmstereo package",
+)
