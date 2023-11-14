c252974b2e4a3de682459863c6659a71c2e5d78e
Matias Mattamala <matias@robots.ox.ac.uk>
2023-11-14 11:23:35 +0000

    Minor module fixes

--- mmstereo/setup.py
+++ mmstereo/setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name="mmstereo",
-    version="0.0.0",
+    version="0.0.1",
     author="Krishna Shankar, Mark Tjersland, Jeremy Ma, Kevin Stone, Max Bajracharya",
     author_email="matias@robots.ox.ac.uk",
-    packages=["mmstereo"],
+    packages=find_packages(),
     package_dir={"": "."},
     python_requires=">=3.8",
     description="mmstereo package",
